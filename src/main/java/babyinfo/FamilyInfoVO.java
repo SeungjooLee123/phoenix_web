6793b028b08edd2464d3d50bc4a988ce79c06911
eor8451 <77501171+eor8451@users.noreply.github.com>
2022-03-21 17:53:28 +0900

    my 디비 연동

---
+++ src/main/java/babyinfo/FamilyInfoVO.java
@@ -0,0 +1,29 @@
+package babyinfo;
+
+public class FamilyInfoVO {
+	String title, id, family_rels;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFamily_rels() {
+		return family_rels;
+	}
+
+	public void setFamily_rels(String family_rels) {
+		this.family_rels = family_rels;
+	}
+}
