af5693c93c8f94dcadd1f06bc0af708430d759bd
gyu-hyung <94032808+gyu-hyung@users.noreply.github.com>
2022-03-22 16:33:34 +0900

    중복확인 끗

--- src/main/java/com/phoenix/bss/JoinController.java
+++ src/main/java/com/phoenix/bss/JoinController.java
@@ -1,5 +1,31 @@
 package com.phoenix.bss;
 
-public class JoinController {
+import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import join.JoinDAO;
+
+@Controller
+public class JoinController {
+	@Autowired JoinDAO dao;
+	
+	
+	//ID 중복확인
+	@ResponseBody
+	@RequestMapping ( value = "/id_check.join", produces="application/json;charset=UTF-8")
+	public String id_check(String id) throws IOException {
+		Gson gson = new Gson();
+		System.out.println(id);
+		return gson.toJson( dao.id_check(id) );
+	}
+	
+	
+	
+	
 }
