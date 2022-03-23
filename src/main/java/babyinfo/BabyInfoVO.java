112ea017acf5c7eadb5a5f5404e081c2f106b8e0
eor8451 <77501171+eor8451@users.noreply.github.com>
2022-03-23 13:56:30 +0900

    최근 몸무게, 키 조회

--- src/main/java/babyinfo/BabyInfoVO.java
+++ src/main/java/babyinfo/BabyInfoVO.java
@@ -4,7 +4,6 @@ import java.sql.Date;
 
 public class BabyInfoVO {
 	String baby_id, title, baby_name, baby_photo, baby_gender, id;
-	double baby_kg, baby_cm;
 	Date baby_birth;
 	
 	public String getId() {
@@ -43,25 +42,10 @@ public class BabyInfoVO {
 	public void setBaby_gender(String baby_gender) {
 		this.baby_gender = baby_gender;
 	}
-	public double getBaby_kg() {
-		return baby_kg;
-	}
-	public void setBaby_kg(double baby_kg) {
-		this.baby_kg = baby_kg;
-	}
-	public double getBaby_cm() {
-		return baby_cm;
-	}
-	public void setBaby_cm(double baby_cm) {
-		this.baby_cm = baby_cm;
-	}
 	public Date getBaby_birth() {
 		return baby_birth;
 	}
 	public void setBaby_birth(Date baby_birth) {
 		this.baby_birth = baby_birth;
 	}
-	
-	
-
 }
--- src/main/java/com/phoenix/bss/BabyStorController.java
+++ src/main/java/com/phoenix/bss/BabyStorController.java
@@ -1,8 +1,28 @@
 package com.phoenix.bss;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import storage.BabyStorDAO;
+import storage.BabyStorVO;
 
 @Controller
 public class BabyStorController {
-
+	@Autowired BabyStorDAO dao;
+	
+	Gson gson = new Gson();
+	
+	@ResponseBody
+	@RequestMapping(value = "/cntbody.stor", produces = "application/json;charset=UTF-8")
+	public String cnt_baby(String baby_id) {
+		BabyStorVO vo = dao.cntBody(baby_id);
+		if(vo == null) {
+			return gson.toJson("키, 몸무게 기록이 없습니다.");
+		}
+		return gson.toJson(vo.getStor_cm() + "cm, " + vo.getStor_kg() + "kg");
+	}
 }
