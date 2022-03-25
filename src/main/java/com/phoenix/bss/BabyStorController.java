477bd7f86442cc336ed38a76f23f3a0558df83ef
SeungjooLee123 <77609889+SeungjooLee123@users.noreply.github.com>
2022-03-25 17:54:11 +0900

    중간111

--- src/main/java/com/phoenix/bss/BabyStorController.java
+++ src/main/java/com/phoenix/bss/BabyStorController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import storage.BabyStorDAO;
 import storage.BabyStorVO;
@@ -14,7 +15,17 @@ import storage.BabyStorVO;
 public class BabyStorController {
 	@Autowired BabyStorDAO dao;
 	
-	Gson gson = new Gson();
+	Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+	
+	@ResponseBody
+	@RequestMapping(value = "/cntbody.stor", produces = "application/json;charset=UTF-8")
+	public String cnt_baby(String baby_id) {
+		BabyStorVO vo = dao.cntBody(baby_id);
+		if(vo == null) {
+			return gson.toJson("키, 몸무게 기록이 없습니다.");
+		}
+		return gson.toJson(vo.getStor_cm() + "cm, " + vo.getStor_kg() + "kg");
+	}
 	
 	@ResponseBody
 	@RequestMapping(value = "/cntbody.stor", produces = "application/json;charset=UTF-8")
