dfbb64d8394c23ab9ab374bdd857c7be20d4f13d
SeungjooLee123 <77609889+SeungjooLee123@users.noreply.github.com>
2022-03-28 17:41:08 +0900

    다이어리 수정 키무게 완료

--- src/main/java/storage/BabyStorDAO.java
+++ src/main/java/storage/BabyStorDAO.java
@@ -1,5 +1,7 @@
 package storage;
 
+import java.util.HashMap;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,5 +13,18 @@ public class BabyStorDAO {
 	//가장 최근 날짜 키, 몸무게 조회
 	public BabyStorVO cntBody(String baby_id) {
 		return sql.selectOne("storage.mapper.cntbody", baby_id);
+	}
+
+	//키 몸무게 넣기
+	public boolean insert(BabyStorVO vo) {
+		return sql.insert("storage.mapper.insert", vo)==1 ? true : false;
+	}
+
+	public BabyStorVO select(HashMap<String, String> map) {
+		return sql.selectOne("storage.mapper.select", map);
+	}
+
+	public boolean update(BabyStorVO vo) {
+		return sql.insert("storage.mapper.update", vo)==1 ? true : false;
 	}
 }
